3177fb188b788536b61c9184f142b4d504617519
nilabh singh <nilabhumeshsingh@gmail.com>
2026-08-20 13:01:54 +0530

    Fix bug in esp

--- cis_security_3d_dashboard/esp32_firmware/esp32_c6_wifi_tracker.ino
+++ cis_security_3d_dashboard/esp32_firmware/esp32_c6_wifi_tracker.ino
@@ -11,6 +11,12 @@
   =============================================================================
 */
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "esp_wpa2.h"
+#include "esp_wifi.h"
+#include "HWCDC.h"
 
 // Create our own HWCDC instance for USB Serial/JTAG output
 HWCDC USBPort;
